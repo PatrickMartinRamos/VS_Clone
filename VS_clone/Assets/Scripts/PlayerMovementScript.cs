f38843889dd6a94e107e6b0d41e438d0ff12f73e
Patrick Ramos <patrick.ramos@ciit.edu.ph>
2023-08-19 14:13:34 +0800

    add system input add player movement using system input

---
+++ VS_clone/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    private Player_input input = null;
+    private Vector2 moveVector = Vector2.zero;
+    private Rigidbody2D rb = null;
+    public float moveSpeed;
+
+    private void Awake()
+    {
+        input = new Player_input();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        rb.velocity = moveVector * moveSpeed;
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.movement.performed += onMovementPerformed;
+        input.Player.movement.canceled += onMovementCancel;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.movement.performed -= onMovementPerformed;
+        input.Player.movement.canceled -= onMovementCancel;
+    }
+
+    private void onMovementPerformed(InputAction.CallbackContext value)
+    {
+        moveVector = value.ReadValue<Vector2>();
+    }
+
+    private void onMovementCancel(InputAction.CallbackContext value)
+    {
+        moveVector = Vector2.zero;
+    }    
+}
